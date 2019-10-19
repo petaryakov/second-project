cf7774063b7c795c347b3175afc036a3d9aa0891
Petar <petaryakov@abv.bg>
2019-10-20 02:00:18 +0300

    changed the greet function

--- greetings.py
+++ greetings.py
@@ -1,8 +1,4 @@
 def greet():
-    print("new line")
-    print("hi eee in new branch")
-    print("hi eee in newNew branch")
-
-def newFunct():
-    print("new func")
+    print("new new Branch")
+   
 
